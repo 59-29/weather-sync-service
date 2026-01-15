19cfec7a0868112b7a4b62fa22581922b7fdfa2a
59-29 <64929036+59-29@users.noreply.github.com>
2026-01-15 19:05:26 +0400

    Added Celery broker and result backend configuration

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery config
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
