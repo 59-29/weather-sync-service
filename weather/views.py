93fa6ee2c5642076e888fe480b3627a471b2e3d6
59-29 <64929036+59-29@users.noreply.github.com>
2026-01-19 19:17:25 +0400

    Update sync endpoint to use Celery task

--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,19 +5,22 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from .models import CityWeather
 from .serializers import CityWeatherSerializer
-from .tasks import sync_weather
+from .tasks import sync_all_weather
+
 
 @api_view(["POST"])
 def trigger_sync(request):
-    task = sync_weather.delay()
+    task = sync_all_weather.delay()
     return Response(
         {"task_id": task.id, "status": "started"},
         status=status.HTTP_202_ACCEPTED,
     )
 
+
 class WeatherList(ListAPIView):
     queryset = CityWeather.objects.all().order_by("id")
     serializer_class = CityWeatherSerializer
+
 
 class WeatherDetail(RetrieveAPIView):
     queryset = CityWeather.objects.all()
